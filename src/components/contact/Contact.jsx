185c80599fe02fe6315480b7708b1853cc82eaa7
richardd82 <richardd82@gmail.com>
2022-07-26 05:15:41 -0500

    Portfolio Template Ready

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './contact.css'
+import {AiOutlineMail} from 'react-icons/ai'
+import {BsMessenger} from 'react-icons/bs'
+import {SiWhatsapp} from 'react-icons/si'
+import {useRef} from 'react'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_30jnl8h', 'template_88gzg4p', form.current, 'jnwL7o8mue7Sjw-zw')
+    
+    e.target.reset();
+  };
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail size={40} className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>richardd82@gmail.com</h5>
+            <a href="mailto:richardd82@gmail.com" target='_blank'>Send a message</a>
+          </article><article className="contact__option">
+            <BsMessenger size={40} className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Ricardo Díaz</h5>
+            <a href="https://m.me/richardd82" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <SiWhatsapp size={40} className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>(+52)5569790254</h5>
+            <a href="https://wa.me/+525569790254" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* End of Contact Option */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Title One',
+    github: 'https://github.com/richardd82',
+    demo: 'https://github.com/richardd82'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Title Two',
+    github: 'https://github.com/richardd82',
+    demo: 'https://github.com/richardd82'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Title Three',
+    github: 'https://github.com/richardd82',
+    demo: 'https://github.com/richardd82'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Title Four',
+    github: 'https://github.com/richardd82',
+    demo: 'https://github.com/richardd82'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Title Five',
+    github: 'https://github.com/richardd82',
+    demo: 'https://github.com/richardd82'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Title Six',
+    github: 'https://github.com/richardd82',
+    demo: 'https://github.com/richardd82'
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+              <article key={id} className="portfolio__items">
+                  <div className="portfolio__items-image">
+                    <img src={image} alt="Pic1" />          
+                  </div>
+                    <h3>{title}</h3>
+                    <div className="portfolio__item-cta">
+                    <a href={github} className='btn' target='__blank'>Github</a>
+                    <a href={demo} className='btn btn-primary' target='__blank'>Live Demo</a>          
+                    </div>
+                </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
